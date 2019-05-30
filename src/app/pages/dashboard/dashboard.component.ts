44e20d37ad1b4f783df55236a7253d58c79da8bd
Sergei Hanka <sergeiahanka@gmail.com>
2019-05-29 20:26:40 -0500

    updated ui

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -22,6 +22,52 @@ export class DashboardComponent implements OnInit {
   public clicked: boolean = true;
   public clicked1: boolean = false;
 
+  stats = [
+    {
+      title: 'Average Retention',
+      abbreviation: 'Av. Retention',
+      value: 50,
+      suffix: "%",
+      color: '#59acfa'
+    },
+    {
+      title: 'Productivity',
+      abbreviation: "Productivity",
+      value: 50,
+      suffix: "%",
+      color: '#febf0e'
+    },
+    {
+      title: 'Average Employee Performance Score',
+      abbreviation: 'Av. Empl Perf.',
+      value: 50,
+      suffix: "pts",
+      color: '#f6851e'
+    },
+    {
+      title: 'Number of Employees Left ?',
+      abbreviation: '# of Empl Left',
+      value: 50,
+      suffix: "",
+      color: '#ef4822'
+    },
+    {
+      title: 'Active Employee Count',
+      abbreviation: 'Active Empl #',
+      value: 50,
+      suffix: "",
+      color: '#8dd26c'
+    },
+    {
+      title: 'New Employee Count',
+      abbreviation: 'New Empl #',
+      value: 50,
+      suffix: "",
+      color: '#806cd2'
+    }
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
@@ -52,10 +98,6 @@ export class DashboardComponent implements OnInit {
 			data: chartExample1.data
 		});
   }
-
-
-
-
 
   public updateOptions() {
     this.salesChart.data.datasets[0].data = this.data;
