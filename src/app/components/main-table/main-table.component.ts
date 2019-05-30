6e776cfd4ab0318f349c67568320ca4ab2d93c7d
Sergei Hanka <sergeiahanka@gmail.com>
2019-05-30 00:43:04 -0500

    updated team ui

--- src/app/components/main-table/main-table.component.ts
+++ src/app/components/main-table/main-table.component.ts
@@ -16,48 +16,29 @@ import {
 })
 export class MainTableComponent implements OnInit {
 
-  public datasets: any;
-  public data: any;
-  public salesChart;
-  public clicked: boolean = true;
-  public clicked1: boolean = false;
+  teams = [
+    {"name": "team1", "rank" : 1 },
+    {"name": "team2", "rank" : 2 },
+    {"name": "team3", "rank" : 3 },
+    {"name": "team4", "rank" : 4 },
+    {"name": "team5", "rank" : 5 },
+    {"name": "team6", "rank" : 6 },
+    {"name": "team7", "rank" : 7 },
+    {"name": "team8", "rank" : 8 },
+    {"name": "team9", "rank" : 9 },
+    {"name": "team10", "rank" : 10 },
+    {"name": "team11", "rank" : 11 },
+    {"name": "team12", "rank" : 12 },
+    {"name": "team13", "rank" : 13 },
+    {"name": "team14", "rank" : 14 },
+    {"name": "team15", "rank" : 15 },
+    {"name": "team16", "rank" : 16 },
+    {"name": "team17", "rank" : 17 },
+    {"name": "team18", "rank" : 18 },
+    {"name": "team19", "rank" : 19 },
+    {"name": "team20", "rank" : 20 }
+  ]
 
-  constructor() { }
-
-  ngOnInit() {
-
-    this.datasets = [
-      [0, 20, 10, 30, 15, 40, 20, 60, 60],
-      [0, 20, 5, 25, 10, 30, 15, 40, 40]
-    ];
-    this.data = this.datasets[0];
-
-
-    var chartOrders = document.getElementById('chart-orders');
-
-    parseOptions(Chart, chartOptions());
-
-
-    var ordersChart = new Chart(chartOrders, {
-      type: 'bar',
-      options: chartExample2.options,
-      data: chartExample2.data
-    });
-
-    var chartSales = document.getElementById('chart-sales');
-
-    this.salesChart = new Chart(chartSales, {
-			type: 'line',
-			options: chartExample1.options,
-			data: chartExample1.data
-    });
-    
-
-  }
-
-  public updateOptions() {
-    this.salesChart.data.datasets[0].data = this.data;
-    this.salesChart.update();
-  }
+  ngOnInit() { }
 
 }
