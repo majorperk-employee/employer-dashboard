a3dea4f0f20e119525f585c4f601e6448af30a4c
Sergei Hanka <sergeiahanka@gmail.com>
2019-05-29 20:26:25 -0500

    updated ui

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,6 +5,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserDropdownComponent } from './user-dropdown/user-dropdown.component';
 
 @NgModule({
   imports: [
@@ -15,7 +16,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   declarations: [
     FooterComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    UserDropdownComponent
   ],
   exports: [
     FooterComponent,
